2dccaeebe9d234bbe22ece1a2168ef29f9b0d8e6
Oğuz Kağan <45708619+khanoguz@users.noreply.github.com>
2022-11-14 21:48:09 +0300

    Add files via upload

---
+++ intro-to-Node/index.js
@@ -0,0 +1,11 @@
+// you can change var later but you cant change const never.!!
+
+//const fileSystem = require("fs");
+//fileSystem.copyFileSync("file1.txt", "file2.txt");
+
+//var superheroes = require("superheroes"); from ndm
+//console.log(superheroes.random());
+
+var supervillains = require("supervillains");
+
+console.log(supervillains.all);
